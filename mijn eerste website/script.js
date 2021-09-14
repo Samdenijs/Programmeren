84dfef5caa6d51886087da6db290bd4204ecba94
Samdenijs <89771087+Samdenijs@users.noreply.github.com>
2021-09-14 12:48:31 +0200

    Initial commit

---
+++ mijn eerste website/script.js
@@ -0,0 +1,4 @@
+// Change heading:
+document.getElementById("myH").innerHTML = "Sam Denijs AEITO21SD-A"; function myFunction() {
+ document.getElementsByTagName("P")[0].innerHTML = "hello World !";
+}
